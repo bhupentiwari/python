8f35ead78718d6f211a7cae8aee85c30863d0629
bhupentiwari <bhupentiwari@live.com>
2024-07-26 00:46:03 +0530

    data types demo

---
+++ 02Section/listdemo.py
@@ -0,0 +1,20 @@
+students = ["Vinay","Bhupen","Janardan","Amey"]
+print(type(students))
+months = ("jan","feb","mar")
+print(len(students))
+print(type(months))
+print(len(months))
+
+print(students[:2])
+print(students[-1])
+
+print(months[:2])
+print(months[-1])
+
+students.append("Mayur")
+
+print(students)
+
+students.insert(0,"Ojas")
+
+print(students)
